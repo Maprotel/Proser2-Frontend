bb9c7cceb0fd4d82f8ce72ef33f2a98609a87623
jorgebarrero <jorgebarrero@gmail.com>
2019-11-19 08:50:35 -0400

    Fix: dashboard refactor app module

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -1,15 +1,18 @@
-
+// Angular
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, ModuleWithProviders } from "@angular/core";
-
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+// Vendor
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { CollapseModule } from "ngx-bootstrap/collapse";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
+
+// Custom modules & services
 import { AuthGuard } from "shared/guards";
 import { AlertService } from "shared/services/helpers/alert.service";
 import { AlertModule } from "shared/modules/alert/alert.module";
@@ -18,22 +21,19 @@ import { ConnectionModule } from "shared/modules/connection/connection.module";
 import { SelectorModule } from "shared/modules/selector/selector.module";
 import { EnvServiceProvider } from "shared/services/helpers/env.service.provider";
 
-import { HomeComponent } from "./pages/home/home.component";
-
-
+import { HeaderModule } from './header/header.module';
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { RedirectModule } from 'shared/modules/redirect/redirect.module';
 
+import { IntroPageModule } from "shared/modules/intro-page/intro-page.module";
 import { HeaderMenuModule } from "shared/modules/header-menu/header-menu.module";
 import { HeaderMenuUserModule } from "shared/modules/header-menu-user/header-menu-user.module";
 import { HeaderMenuBrandModule } from "shared/modules/header-menu-brand/header-menu-brand.module";
-import { IntroPageModule } from "shared/modules/intro-page/intro-page.module";
 
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
-import { HeaderModule } from './header/header.module';
-
-import { RedirectModule } from 'shared/modules/redirect/redirect.module';
+// Components
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from './pages/login/login.component';
 import { ByeComponent } from './pages/bye/bye.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
