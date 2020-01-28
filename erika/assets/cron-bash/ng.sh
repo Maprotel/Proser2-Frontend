bfb93dae3b70956b12e2f040c0cd57056e9bced2
jorgebarrero <jorgebarrero@gmail.com>
2020-01-28 18:33:12 -0400

    Remove: audit

--- erika/assets/cron-bash/ng.sh
+++
@@ -1,2 +0,0 @@
-cd angular
-ng serve
--- erika/assets/cron-bash/pm2.sh
+++
@@ -1,6 +0,0 @@
-SHELL=/bin/bash
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=root
-
-sudo systemctl stop firewalld
-/usr/bin/pm2 kill; sleep 10; /usr/bin/pm2 resurrect
